aa2a2b7c2195d15c77c81c838f5252b034d1c194
David Middlecamp <david@spark.io>
2014-07-07 15:31:06 -0500

    adding license headers

--- js/lib/UserCreator.js
+++ js/lib/UserCreator.js
@@ -1,4 +1,21 @@
-//var PasswordHasher = require('./PasswordHasher');
+/**
+*    Copyright (C) 2013-2014 Spark Labs, Inc. All rights reserved. -  https://www.spark.io/
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License, version 3,
+*    as published by the Free Software Foundation.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+
 var roles = require('./RolesController.js');
 var when = require('when');
 
