49c742796a669e64a44a1d3ac8165e88e5a18053
AntonPuko <antonpukowebdev@gmail.com>
2016-11-30 00:43:37 +0200

    implement UsersController

---
+++ src/lib/controllers/UsersController.js
@@ -0,0 +1,25 @@
+// @flow
+
+import type { Repository, User, UserCredentials } from '../../types';
+
+import Controller from './Controller';
+import httpVerb from '../decorators/httpVerb';
+import route from '../decorators/route';
+
+class UsersController extends Controller {
+  _usersRepository: Repository<User>;
+
+  constructor(usersRepository: Repository<User>) {
+    super();
+    this._usersRepository = usersRepository;
+  }
+
+  @httpVerb('post')
+  @route('/v1/users')
+  async createUser(userCredentials: UserCredentials) {
+    const newUser = await this._usersRepository.create(userCredentials);
+    return this.ok(newUser);
+  }
+}
+
+export default UsersController;
