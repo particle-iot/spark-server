013112243006cd0a33a582d3a93f4e5218eec51d
AntonPuko <antonpukowebdev@gmail.com>
2016-12-16 01:20:07 +0200

    decorators flow annotations

--- src/lib/decorators/allowUpload.js
+++ src/lib/decorators/allowUpload.js
@@ -1,14 +1,20 @@
-import type { Decorator, HttpVerb } from './types';
+// @flow
 
+import type { Decorator, Descriptor } from './types';
+import type Controller from '../controllers/Controller';
+
+/* eslint-disable no-param-reassign */
 export default (
   fileName: string,
   maxCount: number = 0,
-): Decorator => (target, name, descriptor): Object => {
-  const allowedUploads = target[name].allowedUploads || [];
-  allowedUploads.push({
-    maxCount: maxCount ? maxCount : undefined,
-    name: fileName,
-  });
-  target[name].allowedUploads = allowedUploads;
-  return descriptor;
-};
+): Decorator<Controller> =>
+  (target: Controller, name: string, descriptor: Descriptor): Descriptor => {
+    const allowedUploads = target[name].allowedUploads || [];
+    allowedUploads.push({
+      maxCount,
+      name: fileName,
+    });
+
+    target[name].allowedUploads = allowedUploads;
+    return descriptor;
+  };
