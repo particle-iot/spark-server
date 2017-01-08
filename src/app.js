28e06bd0c813e23c730203ba2351b3b1e1d5ecd7
John Kalberer <jlkalberer@gmail.com>
2017-01-08 14:41:43 -0800

    Cleaning app.js

--- src/app.js
+++ src/app.js
@@ -8,31 +8,12 @@ import type {
   NextFunction,
 } from 'express';
 import type {Container} from 'constitute';
-import type { EventPublisher, DeviceServer } from 'spark-protocol';
-import type { Settings } from './types';
+import type {Settings} from './types';
 
 import bodyParser from 'body-parser';
 import express from 'express';
 import morgan from 'morgan';
-
-// Repositories
-import DeviceRepository from './repository/DeviceRepository';
-import {
-  DeviceAttributeFileRepository,
-  DeviceKeyFileRepository,
-} from 'spark-protocol';
-
-// Managers
-import EventManager from './managers/EventManager';
-
-// Routing
 import routeConfig from './RouteConfig';
-import DeviceClaimsController from './controllers/DeviceClaimsController';
-import DevicesController from './controllers/DevicesController';
-import EventsController from './controllers/EventsController';
-import ProvisioningController from './controllers/ProvisioningController';
-import UsersController from './controllers/UsersController';
-import WebhooksController from './controllers/WebhooksController';
 
 export default (
   container: Container,
